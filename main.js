001c1d8c5772fd3e90563eba0d9e92a74e862074
Chaan <rnchagwe27@gmail.com>
2024-06-04 21:50:37 +0800

    2nd if else statement added

--- main.js
+++ main.js
@@ -4,13 +4,28 @@ let wins = 0
 let losses = 0
 let ties = 0
 
-const choice = prompt("Enter rock, paper or scissor: ")
-if (choice !== "rock" && choice !== "paper" && choice !== "scissor") {
-    console.log("Please enter a valid choice.")
+const playerChoice = prompt("Enter rock, paper or scissor: ")
+if (playerChoice !== "rock" && playerChoice !== "paper" && playerChoice !== "scissor") {
+    console.log("Please enter a valid playerChoice.")
 } 
 
 const choices = ["rock", "paper", "scissor"]
 const randomIndex = Math.round(Math.random() * 2)
 const computerChoice = choices[randomIndex]
 
-console.log(computerChoice)
+console.log("Computer chose:", computerChoice)
+
+if (computerChoice === playerChoice) {
+    console.log("Draw!")
+    ties++
+} else if (
+    (playerChoice === "paper" && computerChoice === "rock") ||
+    (playerChoice === "rock" && computerChoice === "scissor") ||
+    (playerChoice === "scissor" && computerChoice === "paper")
+) {
+    console.log("Won")
+    wins++
+} else {
+    console.log("Lost!")
+    losses++
+}
