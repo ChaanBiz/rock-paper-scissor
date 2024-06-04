e14b93d2a350292d7bcc4f7dd5a049004a1d885e
Chaan <rnchagwe27@gmail.com>
2024-06-04 21:57:49 +0800

    done

--- main.js
+++ main.js
@@ -4,28 +4,43 @@ let wins = 0
 let losses = 0
 let ties = 0
 
-const playerChoice = prompt("Enter rock, paper or scissor: ")
-if (playerChoice !== "rock" && playerChoice !== "paper" && playerChoice !== "scissor") {
-    console.log("Please enter a valid playerChoice.")
-} 
+while(true) {
+    const playerChoice = prompt("Enter rock, paper or scissor (q to quit): ")
 
-const choices = ["rock", "paper", "scissor"]
-const randomIndex = Math.round(Math.random() * 2)
-const computerChoice = choices[randomIndex]
+    if (playerChoice.toLocaleLowerCase() === "q") {
+        break;
+    }
 
-console.log("Computer chose:", computerChoice)
+    if (
+        playerChoice !== "rock" && 
+        playerChoice !== "paper" && 
+        playerChoice !== "scissor"
+    ) {
+        console.log("Please enter a valid choice.")
+        continue
+    } 
 
-if (computerChoice === playerChoice) {
-    console.log("Draw!")
-    ties++
-} else if (
-    (playerChoice === "paper" && computerChoice === "rock") ||
-    (playerChoice === "rock" && computerChoice === "scissor") ||
-    (playerChoice === "scissor" && computerChoice === "paper")
-) {
-    console.log("Won")
-    wins++
-} else {
-    console.log("Lost!")
-    losses++
+    const choices = ["rock", "paper", "scissor"]
+    const randomIndex = Math.round(Math.random() * 2)
+    const computerChoice = choices[randomIndex]
+
+    console.log("Computer chose:", computerChoice)
+
+    if (computerChoice === playerChoice) {
+        console.log("Draw!")
+        ties++
+    } else if (
+        (playerChoice === "paper" && computerChoice === "rock") ||
+        (playerChoice === "rock" && computerChoice === "scissor") ||
+        (playerChoice === "scissor" && computerChoice === "paper")
+    ) {
+        console.log("Won")
+        wins++
+    } else {
+        console.log("Lost!")
+        losses++
+    }
 }
+
+
+console.log("Wins:", wins, "Losses:", losses, "Ties:", ties)
